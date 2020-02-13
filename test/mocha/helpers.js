a4dd0e19c9a758f2ff47c095389306e7198c3c3d
Matthew Collier <mcollier@digitalbazaar.com>
2020-02-13 15:55:14 -0500

    Update to latest webkms-client APIs.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -5,15 +5,17 @@
 
 'use strict';
 
+const axios = require('axios');
+const bedrock = require('bedrock');
 const brAccount = require('bedrock-account');
+const brHttpsAgent = require('bedrock-https-agent');
 const database = require('bedrock-mongodb');
 const {promisify} = require('util');
 const uuid = require('uuid/v4');
+const {EdvClient} = require('edv-client');
+const {KeystoreAgent, KmsClient} = require('webkms-client');
 
-const api = {};
-module.exports = api;
-
-api.createAccount = email => {
+exports.createAccount = email => {
   const newAccount = {
     id: 'urn:uuid:' + uuid(),
     email
@@ -21,7 +23,7 @@ api.createAccount = email => {
   return newAccount;
 };
 
-api.getActors = async mockData => {
+exports.getActors = async mockData => {
   const actors = {};
   for(const [key, record] of Object.entries(mockData.accounts)) {
     actors[key] = await brAccount.getCapabilities({id: record.account.id});
@@ -29,12 +31,12 @@ api.getActors = async mockData => {
   return actors;
 };
 
-api.prepareDatabase = async mockData => {
-  await api.removeCollections();
+exports.prepareDatabase = async mockData => {
+  await exports.removeCollections();
   await insertTestData(mockData);
 };
 
-api.removeCollections = async (
+exports.removeCollections = async (
   collectionNames = [
     'account',
     'edvConfig',
@@ -47,8 +49,8 @@ api.removeCollections = async (
   }
 };
 
-api.removeCollection =
-  async collectionName => api.removeCollections([collectionName]);
+exports.removeCollection =
+  async collectionName => exports.removeCollections([collectionName]);
 
 async function insertTestData(mockData) {
   const records = Object.values(mockData.accounts);
@@ -64,4 +66,79 @@ async function insertTestData(mockData) {
       throw e;
     }
   }
+}
+
+// the `keystores` endpoint uses session based authentication which is
+// mocked
+exports.createKeystore = async ({capabilityAgent, referenceId}) => {
+  // create keystore
+  const config = {
+    sequence: 0,
+    controller: capabilityAgent.id,
+    invoker: capabilityAgent.id,
+    delegator: capabilityAgent.id
+  };
+  if(referenceId) {
+    config.referenceId = referenceId;
+  }
+  const kmsBaseUrl = `${bedrock.config.server.baseUri}/kms`;
+  const {httpsAgent} = brHttpsAgent;
+  const keystore = await KmsClient.createKeystore({
+    url: `${kmsBaseUrl}/keystores`,
+    config,
+    httpsAgent,
+  });
+  const kmsClient = new KmsClient({httpsAgent});
+  return new KeystoreAgent({capabilityAgent, keystore, kmsClient});
+};
+
+const KMS_MODULE = 'ssm-v1';
+exports.createEdv = async ({
+  capabilityAgent, keystoreAgent, kmsModule = KMS_MODULE, urls
+}) => {
+  // create KAK and HMAC keys for edv config
+  const [keyAgreementKey, hmac] = await Promise.all([
+    keystoreAgent.generateKey({type: 'keyAgreement', kmsModule}),
+    keystoreAgent.generateKey({type: 'hmac', kmsModule})
+  ]);
+
+  // create edv
+  const newEdvConfig = {
+    sequence: 0,
+    controller: capabilityAgent.handle,
+    // TODO: add `invoker` and `delegator` using controllerKey.id *or*, if
+    // this is a profile's edv, the profile ID
+    invoker: capabilityAgent.id,
+    delegator: capabilityAgent.id,
+    keyAgreementKey: {id: keyAgreementKey.id, type: keyAgreementKey.type},
+    hmac: {id: hmac.id, type: hmac.type}
+  };
+
+  const {httpsAgent} = brHttpsAgent;
+  const edvConfig = await EdvClient.createEdv({
+    config: newEdvConfig,
+    httpsAgent,
+    url: urls.edvs,
+  });
+
+  const edvClient = new EdvClient({
+    id: edvConfig.id,
+    keyResolver: _keyResolver,
+    keyAgreementKey,
+    hmac,
+    httpsAgent
+  });
+
+  return {edvClient, edvConfig};
+};
+
+const DEFAULT_HEADERS = {Accept: 'application/ld+json, application/json'};
+// FIXME: make more restrictive, support `did:key` and `did:v1`
+async function _keyResolver({id}) {
+  const {httpsAgent} = brHttpsAgent;
+  const response = await axios.get(id, {
+    headers: DEFAULT_HEADERS,
+    httpsAgent,
+  });
+  return response.data;
 }
