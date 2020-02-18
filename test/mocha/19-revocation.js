35a7b80c6d8e3f70e14e07c768735994ae687b3a
Andrew Jones <ajones@digitalbazaar.com>
2020-02-18 14:42:13 -0500

    Add test util to generate testers for zCap revocation tests.

--- test/mocha/19-revocation.js
+++ test/mocha/19-revocation.js
@@ -3,10 +3,11 @@
  */
 'use strict';
 
-const {config} = require('bedrock');
+const {config, util: {uuid}} = require('bedrock');
 const brEdvStorage = require('bedrock-edv-storage');
 const {AsymmetricKey, CapabilityAgent} = require('webkms-client');
 const {SECURITY_CONTEXT_V2_URL} = require('jsonld-signatures');
+const {CapabilityDelegation} = require('ocapld');
 const database = require('bedrock-mongodb');
 const helpers = require('./helpers');
 const mockData = require('./mock.data');
@@ -15,7 +16,7 @@ const mockData = require('./mock.data');
 const mockEdvId = `${config.server.baseUri}/edvs/z1A2RmqSkhYHcnH1UkZamKF1D`;
 const hashedMockEdvId = database.hash(mockEdvId);
 // all tests involve write
-const expectedAction = 'write';
+const allowedAction = 'write';
 // algorithm required for the jwe headers
 const JWE_ALG = 'ECDH-ES+A256KW';
 // for key generation
@@ -24,86 +25,45 @@ const KMS_MODULE = 'ssm-v1';
 const docId = 'z19pjAGaCdp2EkKzUcvdSf9wG';
 
 describe('revocation API', function() {
-  let actors, accounts = null;
-  // first create 3 testers alice, bob, and carol
-  const testers = {
-    // alice is the rootCapability
-    alice: {
-      email: 'alice@example.com',
-      secret: '40762a17-1696-428f-a2b2-ddf9fe9b4987',
-      handle: 'aliceKey',
-      capabilityAgent: null,
-      keystoreAgent: null,
-      keyAgreementKey: null,
-      actor: null,
-      account: null
-    },
-    // bob is delegated write access to alice's EDV
-    bob: {
-      email: 'bob@example.com',
-      secret: '34f2afd1-34ef-4d46-a998-cdc5462dc0d2',
-      handle: 'bobKey',
-      capabilityAgent: null,
-      keystoreAgent: null,
-      actor: null,
-      account: null
-    },
-    // bob delegates his write capability for alice's EDV to carol
-    carol: {
-      email: 'carol@example.com',
-      secret: 'ae806cd9-2765-4232-b955-01e1024ac032',
-      handle: 'carolKey',
-      capabilityAgent: null,
-      keystoreAgent: null,
-      actor: null,
-      account: null
+  // TODO: Rename this.
+  // TODO: Move this to helpers.
+  const makeTestData = async ({testers = []}) => {
+    const actors = await helpers.getActors(mockData);
+    const accounts = mockData.accounts;
+    const testData = {};
+    for(const tester of testers) {
+      const email = `${tester}@example.com`;
+      const testerData = testData[tester] = {
+        email,
+        secret: uuid(),
+        handle: `${tester}Key`,
+        actor: actors[email],
+        account: accounts[email]
+      };
+      testerData.capabilityAgent = await CapabilityAgent.fromSecret({
+        secret: testerData.secret,
+        handle: testerData.handle
+      });
+      testerData.keystoreAgent = await helpers.createKeystore({
+        capabilityAgent: testerData.capabilityAgent,
+        referenceId: testerData.secret
+      });
+      testerData.keyAgreementKey = await testerData.keystoreAgent.
+        generateKey({type: 'keyAgreement', kmsModule: KMS_MODULE});
+      testerData.verificationKey = await testerData.keystoreAgent.
+        generateKey(
+          {type: 'Ed25519VerificationKey2018', kmsModule: KMS_MODULE});
     }
+    return testData;
   };
+  let testers = null;
 
   beforeEach(async function() {
-    await helpers.prepareDatabase(mockData);
-    // {alice, bob, carol} @example.com are in here.
-    actors = await helpers.getActors(mockData);
-    accounts = mockData.accounts;
-    testers.alice.capabilityAgent = await CapabilityAgent.fromSecret({
-      secret: testers.alice.secret,
-      handle: testers.alice.handle
-    });
-    testers.alice.keystoreAgent = await helpers.createKeystore({
-      capabilityAgent: testers.alice.capabilityAgent,
-      referenceId: testers.alice.secret
-    });
-    testers.alice.keyAgreementKey = await testers.alice.keystoreAgent.
-      generateKey({type: 'keyAgreement', kmsModule: KMS_MODULE});
-    testers.alice.actor = actors[testers.alice.email];
-    testers.alice.account = accounts[testers.alice.email];
-    testers.bob.capabilityAgent = await CapabilityAgent.fromSecret({
-      secret: testers.bob.secret,
-      handle: testers.bob.handle
-    });
-    testers.bob.keystoreAgent = await helpers.createKeystore({
-      capabilityAgent: testers.bob.capabilityAgent,
-      referenceId: testers.bob.secret
-    });
-    testers.bob.keyAgreementKey = await testers.bob.keystoreAgent.
-      generateKey({type: 'keyAgreement', kmsModule: KMS_MODULE});
-    testers.bob.actor = actors[testers.bob.email];
-    testers.bob.account = accounts[testers.bob.email];
-    testers.carol.capabilityAgent = await CapabilityAgent.fromSecret({
-      secret: testers.carol.secret,
-      handle: testers.carol.handle
-    });
-    testers.carol.keystoreAgent = await helpers.createKeystore({
-      capabilityAgent: testers.carol.capabilityAgent,
-      referenceId: testers.carol.secret
-    });
-    testers.carol.actor = actors[testers.carol.email];
-    testers.carol.account = accounts[testers.carol.email];
-    console.log({testers});
+    // first create 3 testers alice, bob, and carol
+    testers = await makeTestData({testers: ['alice', 'bob', 'carol']});
   });
 
   it('should delegate & revoke write access', async function() {
-    // alice delegates to bob with bob as a delegator
     // bob delegates his write capability from alice to carol.
     // all 3 of them are able to write to the same EDV.
     // Bob revoke's carol's capability
@@ -144,8 +104,16 @@ describe('revocation API', function() {
         ]
       }
     };
+    // alice delegates a `write` capability to bob with bob as a delegator
+    const writeZcap = {
+      id: 'urn:zcap:delegate:alice:bob',
+      '@context': SECURITY_CONTEXT_V2_URL,
+      allowedAction,
+      invoker: testers.bob.verificationKey.id,
+      delegator: testers.bob.verificationKey.id
+    };
     let record = await brEdvStorage.insert({
-      actor: actors['alice@example.com'],
+      actor: testers.alice.actor,
       edvId: mockEdvId,
       doc,
     });
