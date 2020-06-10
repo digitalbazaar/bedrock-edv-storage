c145a245c90273f4d1c4d6c542066a6d3466fcd4
Mandy Venables <mandylvenables@gmail.com>
2020-06-10 18:48:23 -0400

    Added isSafeInteger check.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -57,7 +57,7 @@ exports.sequenceNumberTests = [
   ['in range [2**32, MAX_SAFE_INTEGER] (middle)',
     2 ** 32 + (Number.MAX_SAFE_INTEGER - 2 ** 32 - 1) / 2],
   ['in range [2**32, MAX_SAFE_INTEGER] (random)',
-    getRandomIntInclusive(2 ** 32, Number.MAX_SAFE_INTEGER)]
+    getRandomIntInclusive(2 ** 32, Number.MAX_SAFE_INTEGER - 1)]
 ].map(d => ({
   title: `should insert a document with sequence number ${d[0]}`,
   updateTitle: `should update a document with sequence number ${d[0]}`,
