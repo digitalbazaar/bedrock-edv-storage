cd2147d889fe25a3e8a3f81e1e2e81f7f717872b
Matthew Collier <mcollier@digitalbazaar.com>
2020-09-28 11:49:12 -0400

    Use bedrock-mongodb@8.

--- test/test.config.js
+++ test/test.config.js
@@ -16,7 +16,6 @@ config.mocha.tests.push(path.join(__dirname, 'mocha'));
 config.mongodb.name = 'bedrock_edv_storage_test';
 config.mongodb.host = 'localhost';
 config.mongodb.port = 27017;
-config.mongodb.local.collection = 'bedrock_edv_storage_test';
 // drop all collections on initialization
 config.mongodb.dropCollections = {};
 config.mongodb.dropCollections.onInit = true;
