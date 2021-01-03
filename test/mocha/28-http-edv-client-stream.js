49a2778ce0aa71d6f068e0a5c1579bdc83945ac4
Andrew L Jones <ajones@digitalbazaar.com>
2021-01-03 15:27:49 -0500

    Add first working streams test.

---
+++ test/mocha/28-http-edv-client-stream.js
@@ -0,0 +1,233 @@
+/*!
+ * Copyright (c) 2018-2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {ReadableStream} = require('web-streams-polyfill/ponyfill');
+const bedrock = require('bedrock');
+const helpers = require('./helpers');
+const mockData = require('./mock.data');
+const {config} = bedrock;
+const {CapabilityAgent} = require('webkms-client');
+const {EdvClient, EdvDocument} = require('edv-client');
+
+let actors;
+let urls;
+
+// creates a unit8 array of variable size
+// that can be used as stream data
+function getRandomUint8({size = 50} = {}) {
+  return new Uint8Array(size).map(
+    // 255 is the max value of a Unit8
+    () => Math.floor(Math.random() * 255));
+}
+
+describe('bedrock-edv-storage HTTP API - edv-client chunks', function() {
+  let passportStub;
+  let capabilityAgent;
+  let edvClient;
+  let invocationSigner;
+  before(async () => {
+    await helpers.prepareDatabase(mockData);
+    actors = await helpers.getActors(mockData);
+    passportStub = helpers.stubPassport({actor: actors['alpha@example.com']});
+    // common URLs
+    const {baseUri} = config.server;
+    const root = `${baseUri}/edvs`;
+    urls = {
+      edvs: root,
+    };
+    const secret = '9c727b65-8553-4275-9ac3-0ac89396efc0';
+    const handle = 'testKey3';
+    capabilityAgent = await CapabilityAgent.fromSecret(
+      {secret, handle});
+    invocationSigner = capabilityAgent.getSigner();
+    const keystoreAgent = await helpers.createKeystore({capabilityAgent});
+
+    // corresponds to the passport authenticated user
+    const actor = actors['alpha@example.com'];
+
+    ({edvClient} = await helpers.createEdv(
+      {actor, capabilityAgent, keystoreAgent, urls}));
+  });
+
+  after(() => {
+    passportStub.restore();
+  });
+
+  it('should insert a document with a stream', async () => {
+    const testId = await EdvClient.generateId();
+    const doc = {id: testId, content: {someKey: 'someValue'}};
+    const data = getRandomUint8();
+    const stream = new ReadableStream({
+      pull(controller) {
+        controller.enqueue(data);
+        controller.close();
+      }
+    });
+    const inserted = await edvClient.insert(
+      {invocationSigner, doc, stream});
+
+    // Streams are added in an update
+    // after the initial document has been written
+    // hence the sequence is 1 and not 0.
+    inserted.content.should.deep.equal({someKey: 'someValue'});
+    should.exist(inserted.stream);
+    inserted.stream.should.be.an('object');
+  });
+
+  it('should be able to decrypt a stream from an EdvDocument', async () => {
+    edvClient.ensureIndex({attribute: 'content.indexedKey'});
+    const testId = await EdvClient.generateId();
+    const doc = {id: testId, content: {indexedKey: 'value1'}};
+    const data = getRandomUint8();
+    const stream = new ReadableStream({
+      pull(controller) {
+        controller.enqueue(data);
+        controller.close();
+      }
+    });
+    await edvClient.insert({doc, stream, invocationSigner});
+    const edvDoc = new EdvDocument({
+      invocationSigner,
+      id: doc.id,
+      keyAgreementKey: edvClient.keyAgreementKey,
+      capability: {
+        id: `${edvClient.id}`,
+        invocationTarget: `${edvClient.id}/documents/${doc.id}`
+      }
+    });
+    const result = await edvDoc.read();
+    result.should.be.an('object');
+    result.content.should.eql({indexedKey: 'value1'});
+    should.exist(result.stream);
+    result.stream.should.be.an('object');
+    const expectedStream = await edvDoc.getStream({doc: result});
+    const reader = expectedStream.getReader();
+    let streamData = new Uint8Array(0);
+    let done = false;
+    while(!done) {
+      // value is either undefined or a Uint8Array
+      const {value, done: _done} = await reader.read();
+      // if there is a chunk then we need to update the streamData
+      if(value) {
+        // create a new array with the new length
+        const next = new Uint8Array(streamData.length + value.length);
+        // set the first values to the existing chunk
+        next.set(streamData);
+        // set the chunk's values to the rest of the array
+        next.set(value, streamData.length);
+        // update the streamData
+        streamData = next;
+      }
+      done = _done;
+    }
+  });
+  it('should be able to write a stream to an EdvDocument', async () => {
+    edvClient.ensureIndex({attribute: 'content.indexedKey'});
+    const docId = await EdvClient.generateId();
+    const doc = {id: docId, content: {indexedKey: 'value2'}};
+    await edvClient.insert({doc, invocationSigner});
+    const edvDoc = new EdvDocument({
+      invocationSigner,
+      id: doc.id,
+      keyAgreementKey: edvClient.keyAgreementKey,
+      capability: {
+        id: `${edvClient.id}`,
+        invocationTarget: `${edvClient.id}/documents/${doc.id}`
+      }
+    });
+    const data = getRandomUint8();
+    const stream = new ReadableStream({
+      pull(controller) {
+        controller.enqueue(data);
+        controller.close();
+      }
+    });
+    const result = await edvDoc.write({
+      doc, stream, invocationSigner});
+    result.should.be.an('object');
+    result.content.should.deep.equal({indexedKey: 'value2'});
+    should.exist(result.stream);
+    const expectedStream = await edvDoc.getStream({doc: result});
+    const reader = expectedStream.getReader();
+    let streamData = new Uint8Array(0);
+    let done = false;
+    while(!done) {
+      // value is either undefined or a Uint8Array
+      const {value, done: _done} = await reader.read();
+      // if there is a chunk then we need to update the streamData
+      if(value) {
+        // create a new array with the new length
+        const next = new Uint8Array(streamData.length + value.length);
+        // set the first values to the existing chunk
+        next.set(streamData);
+        // set the chunk's values to the rest of the array
+        next.set(value, streamData.length);
+        // update the streamData
+        streamData = next;
+      }
+      done = _done;
+    }
+  });
+  it('should throw error if document chunk does not exist', async () => {
+    edvClient.ensureIndex({attribute: 'content.indexedKey'});
+    const docId = await EdvClient.generateId();
+    const doc = {id: docId, content: {indexedKey: 'value3'}};
+    const data = getRandomUint8();
+    const stream = new ReadableStream({
+      pull(controller) {
+        controller.enqueue(data);
+        controller.close();
+      }
+    });
+    await edvClient.insert({doc, invocationSigner, stream});
+    const edvDoc = new EdvDocument({
+      invocationSigner,
+      id: doc.id,
+      keyAgreementKey: edvClient.keyAgreementKey,
+      capability: {
+        id: `${edvClient.id}`,
+        invocationTarget: `${edvClient.id}/documents/${doc.id}`
+      }
+    });
+    const result = await edvDoc.read();
+
+    result.should.be.an('object');
+    result.content.should.eql({indexedKey: 'value3'});
+    should.exist(result.stream);
+    result.stream.should.be.an('object');
+
+    // intentionally clear the database for chunks
+    mock.edvStorage.chunks.clear();
+
+    const expectedStream = await edvDoc.getStream({doc: result});
+    const reader = expectedStream.getReader();
+    let streamData = new Uint8Array(0);
+    let done = false;
+    let err;
+    try {
+      while(!done) {
+        // value is either undefined or a Uint8Array
+        const {value, done: _done} = await reader.read();
+        // if there is a chunk then we need to update the streamData
+        if(value) {
+          // create a new array with the new length
+          const next = new Uint8Array(streamData.length + value.length);
+          // set the first values to the existing chunk
+          next.set(streamData);
+          // set the chunk's values to the rest of the array
+          next.set(value, streamData.length);
+          // update the streamData
+          streamData = next;
+        }
+        done = _done;
+      }
+    } catch(e) {
+      err = e;
+    }
+    should.exist(err);
+    err.name.should.equal('NotFoundError');
+    err.message.should.equal('Document chunk not found.');
+  });
+});
