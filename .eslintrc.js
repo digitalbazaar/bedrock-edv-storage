2be56a20d5e0cd8c85b3260047735180e84a7c69
Dave Longley <dlongley@digitalbazaar.com>
2022-03-29 13:38:30 -0400

    Update linting and CI configs.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
+  root: true,
+  parserOptions: {
+    // this is required for dynamic import()
+    ecmaVersion: 2020
+  },
   env: {
     node: true
   },
-  extends: ['digitalbazaar'],
-  root: true,
+  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
   ignorePatterns: ['node_modules/']
 };
