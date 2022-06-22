1e4df63953b5a7a404b6122be2b706fb90029080
David I. Lehn <dlehn@digitalbazaar.com>
2022-06-22 00:40:09 -0400

    Lint module.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    // this is required for dynamic import()
-    ecmaVersion: 2020
-  },
   env: {
     node: true
   },
-  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
+  extends: [
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/module'
+  ],
   ignorePatterns: ['node_modules/']
 };
